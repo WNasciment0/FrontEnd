4925bd8fdf4bdbba8d6b1d22b50c5535aba61e10
WNasciment0 <116725142+WNasciment0@users.noreply.github.com>
2022-11-30 11:34:44 -0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,76 @@
+"use strict";
+class Produto{
+    constructor(codigo,nome,imagem,promocao,preco,desconto){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.imagem = imagem;
+        this.promocao = promocao;
+        this.preco = preco;
+        this.desconto = desconto;
+}
+lista(products){
+    var lista=document.getElementById("lista");
+    for(var i=0; i < products.length; i++){
+let produto = new Produto(products[i].codigo,products[i].nome,products[i].imagem,products[i].promocao,products[i].preco,products[i].desconto,);
+var column, thumbnail, title, image, old_price, price, btn;
+column = document.createElement("div");
+thumbnail = document.createElement("div");
+title = document.createElement("h1");
+image = document.createElement("img");
+old_price = document.createElement("small");
+price = document.createElement("p");
+btn = document.createElement("a");
+column.classList.add("col-xs-12");
+column.classList.add("col-sm-6");
+column.classList.add("col-md-4");
+thumbnail.classList.add("thumbnail");
+title.classList.add("text-center");
+old_price.classList.add("old-price");
+price.classList.add("price");
+btn.classList.add("btn");
+btn.classList.add("btn-primary");
+btn.innerHTML = "Comprar";
+title.innerHTML = produto.nome;
+image.setAttribute("src", produto.imagem);
+if(produto.promocao == true){ old_price.innerHTML = "R$" + produto.preco; price.innerHTML = "R$" + (produto.preco - produto.desconto);}
+else{old_price.setAttribute("display","nome");price.innerHTML = "R$" + produto.preco;}
+thumbnail.appendChild(title);
+thumbnail.appendChild(image);
+thumbnail.appendChild(old_price);
+thumbnail.appendChild(price);
+thumbnail.appendChild(btn);
+column.appendChild(thumbnail);
+lista.appendChild(column)
+        }
+    }
+}
+
+var products = [
+    {
+        codigo: 1,
+        nome: "Lingerie Marrom",
+        imagem:"img/img1.png",
+        promocao: true,
+        preco: 170.00,
+        desconto: 0,
+    },
+    {
+        codigo: 2,
+        nome: "Lingerie Vermelho",
+        imagem:"img/img2.png",
+        promocao: true,
+        preco: 150.00,
+        desconto: 0,
+    },
+    {
+        codigo: 3,
+        nome: "Lingerie Branco",
+        imagem:"img/img3.png",
+        promocao: true,
+        preco: 165.00,
+        desconto: 0,
+    },
+]
+
+let produto = new Produto();
+produto.lista(products);
